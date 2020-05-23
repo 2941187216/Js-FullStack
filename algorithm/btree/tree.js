48a812982595dd97749caecb8455a957a55e502e
2941187216 <2941187216@qq.com>
2020-05-23 19:01:55 +0800

    算法

---
+++ algorithm/btree/tree.js
@@ -0,0 +1,16 @@
+let tree = {
+    val:'F',
+    left:{
+        val:'C',
+        left:{
+            val:'A'
+        },
+        right:{
+            val:'D'
+        },
+    },
+    right:{
+        val:'E'
+    }
+}
+// tree.left.left
