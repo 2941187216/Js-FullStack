aa8694132ab6c879d22b4e50723af57754959fa0
2941187216 <2941187216@qq.com>
2020-06-12 21:06:39 +0800

    high-order-function

---
+++ js/high-order-function/add.js
@@ -0,0 +1,10 @@
+function buildUrl(url, obj) {
+    // 拼接
+    // 内部的逻辑是公用的
+    // 不同的地方，通过参数传进来
+}
+
+buildUrl('http://localhost',{search: '海阔天空'});
+buildUrl('http://api.com/v1',{ search: '海阔天空'});
+// 1. 不同
+// 2. 公用
