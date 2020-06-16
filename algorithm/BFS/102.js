83f5ddfc3d53ec7ad3669e3b0537b869b08ac082
2941187216 <2941187216@qq.com>
2020-06-16 22:21:19 +0800

    BFC

--- algorithm/BFS/102.js
+++ algorithm/BFS/102.js
@@ -15,3 +15,17 @@ var levelOrder = function(root) {
     }
     return res;
 }
+
+var levelOrder = function(root) {
+  let res = []
+  function walk(node, level) {
+    if (!node) return;
+    let val = node.val;
+    if (!res[level]) res.push([]);
+    res[level].push(val);
+    walk(node.left, level + 1);
+    walk(node.right, level + 1);
+  }
+  walk(root, 0);
+  return res;
+}
