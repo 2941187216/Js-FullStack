24f5df76ec212034e6c7528d258da52fd6ed9402
2941187216 <2941187216@qq.com>
2020-06-25 21:20:46 +0800

    package.json

--- 算法/树/104.js
+++
@@ -1,9 +0,0 @@
-var maxDepth = function(root) {
-    if(root) {
-        let left = maxDepth(root.left);
-        let right = maxDepth(root.right);
-        let depth = Math.max(left, right) + 1;
-        return depth;
-    }
-    return 0;
-}
