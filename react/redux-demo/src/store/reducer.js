d0925209cf6a9aa1124284fc619066ccab27a0e5
2941187216 <2941187216@qq.com>
2020-06-25 21:19:17 +0800

    redux-demo

---
+++ react/redux-demo/src/store/reducer.js
@@ -0,0 +1,12 @@
+const defaultState = {
+    inputValue: 'Write Something',
+    list: [
+        '早8点开晨会，分配今天任务',
+        '早9点开需求沟通会',
+        '早9点开需求沟通会'
+    ]
+}
+
+export default (state = defaultState, action) => {
+    return state
+}
