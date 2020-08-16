64d511866b1b15dc6cd02b09a32930cf41f4f259
2941187216 <2941187216@qq.com>
2020-08-16 22:39:16 +0800

    flat

---
+++ 壹题/11.flat/11.js
@@ -0,0 +1,7 @@
+
+let arr = [ [1, 2, 2], [3, 4, 5, 5], [6, 7, 8, 9, [11, 12, [12, 13, [14] ] ] ], 10];
+function help(arr) {
+    return Array.from(new Set(arr.flat(Infinity))).sort((a, b) => a - b);
+}
+
+console.log(help(arr))
