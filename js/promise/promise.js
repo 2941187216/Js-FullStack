af1691679986a2bd8047fc16664904f418b55252
2941187216 <2941187216@qq.com>
2020-08-17 22:53:52 +0800

    promise

--- js/promise/promise.js
+++ js/promise/promise.js
@@ -5,17 +5,27 @@ class HD {
     constructor(executor){
         this.status = HD.PENDING;
         this.value = null;
-        executor(this.resolve.bind(this), this.reject.bind(this))
+       try {
+            executor(this.resolve.bind(this), this.reject.bind(this))
+       } catch (error) {
+           this.reject(error)
+       }
     }
     resolve(value) {
-        console.log(this);
-        this.status = HD.FUFILLED;
-        this.value = value;
+        // console.log(this);
+        if (this.status === HD.PENDING) {
+            this.status = HD.FUFILLED;
+            this.value = value;
+        }
+       
     }
     reject(reason) {
-        console.log(this);
-        this.status = HD.REJECTED;
-        this.value = value;
+        // console.log(this);
+        if (this.status === HD.REJECTED) {
+            this.status = HD.REJECTED;
+            this.value = reason;
+        }
+        
     }
 }
 
