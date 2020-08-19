e68416b49ebcc5ea6576304b11902dc1d9f0735a
2941187216 <2941187216@qq.com>
2020-08-19 18:17:11 +0800

    shallow

---
+++ js/clone/shallow.js
@@ -0,0 +1,21 @@
+
+function shallow(o) {
+    let obj = {};
+    for (let i in o) {
+        obj[i] = o[i];
+    }
+    return obj;
+}
+
+oldobj = {
+    a: 1,
+    b: ['e', 'f', 'g'],
+    c: { h: { i : 2}}
+}
+
+const newobj = shallow(oldobj);
+console.log(newobj.c.h, oldobj.c.h);
+console.log(newobj.c.h === oldobj.c.h);
+
+newobj.c.h = 'change'
+console.log(newobj.c.h, oldobj.c.h)
